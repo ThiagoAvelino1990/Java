d7cfd2c2276fdbf6f0b0e93e3818d0272ab1495a
Thiago Avelino <thiagoavelinoalves@hotmail.com>
2026-05-18 01:02:41 -0300

    Meu primeiro programa java. Entendendo a classe main

---
+++ MeuPrimeiroPrograma/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,12 @@
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args){
+        /**
+         * Compilando o programa no terminal
+         * javac MeuPrimeiroPrograma.java
+         * java MeuPrimeiroPrograma Java Thiago
+         */
+        System.out.println("Hello World!");
+
+        System.out.print("programa escrito em " + args[0] + "por " + args[1]);
+    }
+}
